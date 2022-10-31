5ca9e4ca7f7665813f3bcef08a9e4e144797b4cf
Joelminer-satisfactory <joelvanvuuren@gmail.com>
2022-10-31 19:46:21 +0100

    implemented dotnet 6.0 structure

---
+++ SatisfactorySaveParser/SatisfactorySaveParser/FSaveHeader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatisfactorySaveParser
+{
+    internal class FSaveHeader
+    {
+
+        public void Parse(FileStream SaveFile)
+        {
+            // encoding is zlib CINFO(windowsize) 7
+            // encoding is zlib FLEVEL(compression level) 3
+            // visit https://stackoverflow.com/a/54915442/20364636 for additional info
+
+            FileStream OutputStream = File.Create("C:/Users/gerha/AppData/Local/SatisfactorySaveparser-Reloaded/DecompressedSave.txt");
+            var decompressor = new DeflateStream(SaveFile, CompressionMode.Decompress);
+            decompressor.CopyTo(OutputStream);
+        }
+    }
+}
