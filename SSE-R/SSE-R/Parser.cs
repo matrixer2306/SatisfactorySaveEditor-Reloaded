49316ee7c5402544edcf8162486b1f9629be157c
Joelminer-satisfactory <joelvanvuuren@gmail.com>
2023-01-05 21:55:17 +0100

    added new project files with working decompression

---
+++ SSE-R/SSE-R/Parser.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using System.Net;
+
+namespace SSE_R
+{
+    public class Parser
+    {
+        public void Parse(string inputPath, string outputPath)
+        {
+            int count = 0;
+            FileStream inputStream = File.OpenRead(inputPath);
+            Debug.WriteLine(inputStream.Length);
+            while (inputStream.Position < inputStream.Length)
+            {
+                bool foundStart = false;
+                while (!foundStart)
+                {
+                    int b = inputStream.ReadByte();
+                    if (b == -1)
+                    {
+                        throw new Exception("Reached end of file before finding the requested bytes");
+                        // replace with ending of file reading later
+                    }
+                    if (b == 0x78)
+                    {
+                        b = inputStream.ReadByte();
+                        if (b == 0x9c)
+                        {
+                            Debug.WriteLine(inputStream.Position);
+                            foundStart = true;
+                        }
+                    }
+                }
+                using (FileStream decompressedStream = File.Create(Path.Combine(outputPath, $"output{count}.txt")))
+                {
+                    using (DeflateStream decompressionStream = new(inputStream, CompressionMode.Decompress, true))
+                    {
+                        decompressionStream.CopyTo(decompressedStream, 131072);
+                        Debug.WriteLine($"Successfully decompressed the chunk {count}");
+                        count ++;
+                        Debug.WriteLine(inputStream.Position);
+                    }
+                }
+            }
+            inputStream.Close();
+        }
+    }
+}
---
+++ SSE-R/SSE-R/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SSE_R
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Outputs", "Zlib" ,"input.sav");
+            string outputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Outputs", "Zlib");
+
+            Parser p = new();
+            p.Parse(inputPath, outputPath);
+        }
+    }
+}
