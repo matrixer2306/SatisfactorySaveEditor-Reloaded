e0c327466320f5eae9469c8c17a18863458f525c
Joelminer-satisfactory <joelvanvuuren@gmail.com>
2023-01-18 21:18:45 +0100

    Fixed body decompressor
    also made an attempt at outputting header data to a file, mostly works, modMetaData  is currently empty for some reason

--- SSE-R/SSE-R/Parser.cs
+++ SSE-R/SSE-R/Parser.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using System.IO.Compression;
 using System.Text;
+using Windows.Devices.Power;
 
 namespace SSE_R
 {
@@ -9,82 +10,110 @@ namespace SSE_R
         // decompresses chunks and writes the data to an output file
         public void ParseBody(string inputPath, string outputPath)
         {
-            using (FileStream inputStream = new(inputPath, FileMode.Open, FileAccess.Read))
+            FileStream inputStream = new(inputPath, FileMode.Open);
+            FileStream outputStream = File.Create(Path.Combine(outputPath, "Body.bin"));
+
+            byte[] buffer = new byte[4];
+            bool foundStart = false;
+            int totalBytesRead = 0;
+            int chunkCount = 0;
+            int compressedChunkLength = 0;
+            int decompressedChunkLength = 0;
+            int combinedChunkLength = 0;
+            long chunkStart = 0;
+            int bytesread = 0;
+            long writeStart = 0;
+            long writeEnd = 0;
+            List<long> offsets = new List<long>();
+
+            while (true)
             {
-                using (BinaryReader r = new(inputStream, Encoding.Unicode, true))
+                Debug.WriteLine($"Starting search for C1 83 2A 9E at {inputStream.Position}");
+                while (!foundStart)
                 {
-                    // initialising variables
-                    int count = 0;
-                    long outputPosition = 0;
-                    bool foundStart = false;
-                    int nextChunkSize = 0;
-                    int b = 0;
-                    inputStream.Position = 0;
-                    int outputSize = 0;
-                    // loops through all the chunks, stopping when the catch is triggered
-                    while (true)
+                    var b = inputStream.ReadByte();
+                    switch (b)
                     {
-                        while (!foundStart)
-                        {
-                            // catches System.IO.EndOfStreamException to close the stream and write debug data when the end is reached
+                        case -1:
+                            Debug.WriteLine($"Input stream length is {inputStream.Length}\n Read {totalBytesRead} bytes from {chunkCount} chunks.");
+                            if (totalBytesRead < combinedChunkLength)
+                            {
+                                Debug.WriteLine($"Should have written {combinedChunkLength} bytes, was off by {combinedChunkLength - totalBytesRead}");
+                            }
+                            inputStream.Close();
                             try
                             {
-                                b = r.ReadByte();
+                                throw new Exception("reached the end of the stream");
                             }
-                            catch (EndOfStreamException)
+                            catch (Exception)
                             {
-                                Debug.WriteLine("Reached the end of the stream");
-                                Debug.WriteLine(count, "count");
-                                Debug.WriteLine(nextChunkSize, "last chunk size = ");
-                                Debug.WriteLine(outputSize, "body.txt size in bytes should be ");
                                 return;
                             }
-                            // find location of the chunk header, finds the length of the chunk and moves the stream to the beginning of the compressed data
-                            if (b == 0xC1)
+                        case 0xC1:
+                            b = inputStream.ReadByte();
+                            if (b == 0xc1)
                             {
-                                b = r.ReadByte();
-                                if (b == 0x83)
+                                b = inputStream.ReadByte();
+                            }
+                            if (b == 0x83)
+                            {
+                                b = inputStream.ReadByte();
+                                if (b == 0x2A)
                                 {
-                                    b = r.ReadByte();
-                                    if (b == 0x2A)
+                                    b = inputStream.ReadByte();
+                                    if (b == 0x9E)
                                     {
-                                        b = r.ReadByte();
-                                        if (b == 0x9E)
-                                        {
-                                            foundStart = true;
-                                            r.BaseStream.Seek(20, SeekOrigin.Current);
-                                            nextChunkSize = r.ReadInt32();
-                                            r.BaseStream.Seek(22, SeekOrigin.Current);
-                                            inputStream.Position = r.BaseStream.Position;
-                                            outputSize += nextChunkSize;
-                                        }
+                                        inputStream.Seek(12, SeekOrigin.Current);
+                                        inputStream.Read(buffer, 0, 4);
+                                        compressedChunkLength = BitConverter.ToInt32(buffer, 0);
+                                        inputStream.Seek(4, SeekOrigin.Current);
+                                        inputStream.Read(buffer, 0, 4);
+                                        decompressedChunkLength = BitConverter.ToInt32(buffer, 0);
+                                        inputStream.Seek(22, SeekOrigin.Current);
+                                        foundStart = true;
+                                        chunkCount++;
+                                        combinedChunkLength += decompressedChunkLength;
+                                        chunkStart = inputStream.Position;
+                                        offsets.Add(chunkStart);
+                                        Debug.WriteLine($"found chunk at {inputStream.Position}");
                                     }
                                 }
                             }
-                        }
-                        // decompresses the chunk body using the nextChunkSize variable as the amount of bytes that are read
-                        using (FileStream outputStream = File.Create(Path.Combine(outputPath, "Body.txt")))
-                        {
-                            using (DeflateStream d = new(inputStream, CompressionMode.Decompress, true))
-                            {
-                                byte[] buffer = new byte[nextChunkSize];
-                                d.Read(buffer, 0, nextChunkSize);
-                                outputStream.Position = outputPosition;
-                                outputStream.Write(buffer, 0, nextChunkSize);
-                                foundStart = false;
-                            }
-                        }
-                        count++;
-                        outputPosition += nextChunkSize;
+                            break;
+                        default:
+                            break;
                     }
+                }
+                // intitialize deflatestream which is disposed after each iteration of the loop
+                using (DeflateStream deflateStream = new(inputStream, CompressionMode.Decompress, true))
+                {
+                    // set position of the deflatestream and log startposition for debugging
+                    deflateStream.BaseStream.Position = chunkStart;
+                    long startposition = deflateStream.BaseStream.Position;
+                    byte[] readData = new byte[decompressedChunkLength];
+                    while (bytesread < decompressedChunkLength)
+                    {
+                        bytesread += deflateStream.Read(readData, bytesread, decompressedChunkLength - bytesread);
+                    }
+                    long endposition = deflateStream.BaseStream.Position;
+                    outputStream.Seek(0, SeekOrigin.End);
+                    writeStart = outputStream.Position;
+                    outputStream.Write(readData, 0, decompressedChunkLength);
+                    writeEnd = outputStream.Position;
+                    inputStream.Position = chunkStart;
+                    foundStart = false;
+                    totalBytesRead += bytesread;
+                    Debug.WriteLine($"Writing debug data for chunk {chunkCount}\n Started reading at offset {startposition} and stopped at offset {endposition}\n The first 4 bytes read are {BitConverter.ToString(readData, 0, 4)}\n Started write at offset {writeStart} and stopped at offset {writeEnd}\n Read {bytesread} bytes and wrote {writeEnd - writeStart} bytes\n ");
+                    bytesread = 0;
                 }
             }
         }
+        
         public void ParseHeader(string inputPath, string outputPath)
         {
             using (FileStream inputStream = File.OpenRead(inputPath))
             {
-                using (FileStream outputStream = File.Create(Path.Combine(outputPath, "Header.txt")))
+                using (FileStream outputStream = File.Create(Path.Combine(outputPath, "Header.bin")))
                 {
                     // set up readers for different encodings
                     BinaryReader readerUTF16 = new(inputStream, Encoding.Default);
@@ -130,18 +159,30 @@ namespace SSE_R
                     // writes all the data gathered from the chunk header to the output file
                     using (BinaryWriter writer = new(outputStream, Encoding.Default))
                     {
-                        writer.Write(headerVersion);
-                        writer.Write(saveVersion);
-                        writer.Write(buildVersion);
-                        writer.Write(mapName);
-                        writer.Write(mapOptions);
-                        writer.Write(sessionID);
-                        writer.Write(playedSeconds);
-                        writer.Write(tickTimeStamp);
-                        writer.Write(sessionVisibility);
-                        writer.Write(unrealVersion);
-                        writer.Write(modMetaData);
-                        writer.Write(modFlags);
+                        Debug.WriteLine(headerVersion);
+                        Debug.WriteLine(saveVersion);
+                        Debug.WriteLine(buildVersion);
+                        Debug.WriteLine(mapName);
+                        Debug.WriteLine(mapOptions);
+                        Debug.WriteLine(sessionID);
+                        Debug.WriteLine(playedSeconds);
+                        Debug.WriteLine(tickTimeStamp);
+                        Debug.WriteLine(sessionVisibility);
+                        Debug.WriteLine(unrealVersion);
+                        Debug.WriteLine(modMetaData);
+                        Debug.WriteLine(modFlags);
+
+                        writer.Write($"Header version = {headerVersion}\n");
+                        writer.Write($"Save version = {saveVersion}\n");
+                        writer.Write($"Build version = {buildVersion}\n");
+                        writer.Write($"Map name = {mapName}\n");
+                        writer.Write($"Map options string: {mapOptions}\n");
+                        writer.Write($"Playtime in seconds = {playedSeconds}\n");
+                        writer.Write($"Tick timestamp = {tickTimeStamp}\n");
+                        writer.Write($"Session visibility = {sessionVisibility}\n");
+                        writer.Write($"unrealVersion = {unrealVersion}\n");
+                        writer.Write($"modMetaData: {modMetaData}\n");
+                        writer.Write($"modFlags: {modFlags}\n");
                     }
                 }
             }
--- SSE-R/SSE-R/Program.cs
+++ SSE-R/SSE-R/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // set input file and output directory, specific output file is specified within the functions
-            string inputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Outputs", "Zlib" ,"input1.sav");
+            string inputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Outputs", "Zlib" ,"input3.sav");
             string outputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Outputs", "Zlib");
 
             Parser p = new();
